f6da8ae9202cd507320feb52f518b4155ba72be4
Michael Rao <jmichaelrao@gmail.com>
2021-06-23 23:12:01 -0500

    add seeds to fill in tables with data

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,14 @@ USE employee_trackerDB;
 
 -- Department table
 CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(30),
     PRIMARY KEY (id),
 );
 
 -- Role table
 CREATE TABLE role (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 0) NOT NULL,
     department_id INT,
@@ -21,7 +21,7 @@ CREATE TABLE role (
 
 -- Employee table
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
---
+++ db/seeds.sql
@@ -0,0 +1,46 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Michael', 'Scott', 1, 8);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Dwight', 'Schrute', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jim', 'Halpert', 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Pam', 'Beesly', 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Ryan', 'Howard', 5, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Andy', 'Bernard', 6, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Toby', 'Flenderson', 7, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jan', 'Levinson', 8, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Oscar', 'Martinez', 9, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Creed', 'Bratton', 10, 1);
+
+INSERT INTO department (department_name)
+VALUES ('Management');
+INSERT INTO department (department_name)
+VALUES ('Sales');
+INSERT INTO department (department_name)
+VALUES ('Reception');
+INSERT INTO department (department_name)
+VALUES ('Accounting');
+INSERT INTO department (department_name)
+VALUES ('Human Resources');
+INSERT INTO department (department_name)
+VALUES ('Quality Assurance');
+
+INSERT INTO role (title, salary, department_id)
+VALUEs ('Regional Manager', 65000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUEs ('Salesman', 60000, 2;
+INSERT INTO role (title, salary, department_id)
+VALUEs ('Accountant', 40000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUEs ('Receptionist', 30000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUEs ('HR Generalist', 40000, 5);
+INSERT INTO role (title, salary, department_id)
+VALUEs ('Regional VP', 200000, 6);
