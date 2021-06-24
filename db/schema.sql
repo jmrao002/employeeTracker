7b3bd1a74d20a97ae5fb86193d389658e0ae6836
Michael Rao <jmichaelrao@gmail.com>
2021-06-23 22:49:40 -0500

    add inquirer questions and cases. add employee schema for all tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,30 @@
 DROP DATABASE IF EXISTS employee_trackerDB;
 CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+-- Department table
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(30),
+    PRIMARY KEY (id),
+);
+
+-- Role table
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 0) NOT NULL,
+    department_id INT,
+    PRIMARY KEY (id),
+)
+
+-- Employee table
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
+)
--- server.js
+++ server.js
@@ -2,3 +2,84 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const cTable = require("console.table");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "employee_trackerDB",
+});
+
+const startPrompt = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "choice",
+        choices: [
+          "View All Employees",
+          "View All Employees By Role",
+          "View all Emplyees By Deparment",
+          "View all Emplyees By Manager",
+          "Add Employee",
+          "Remove Employee",
+          "Update Employee Role",
+          "Update Employee Department",
+          "Update Employee Manager",
+        ],
+      },
+    ])
+    .then(function (val) {
+      switch (val.choice) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+
+        case "View All Employees By Role":
+          viewAllRoles();
+          break;
+
+        case "View all Emplyees By Deparment":
+          viewAllDepartments();
+          break;
+
+        case "View All Employees By Manager":
+          viewAllManagers();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Remove Employee":
+          removeEmployee();
+          break;
+
+        case "Update Employee Role":
+          updateRole();
+          break;
+
+        case "Update Employee Department":
+          updateDepartment();
+          break;
+
+        case "Update Employee Manager":
+          updateManager();
+          break;
+      }
+    });
+};
+
+// view all employees
+const viewAllEmployees = () => {
+  connection.query("");
+};
+
+// connection ID
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as Id" + connection.threadId);
+  startPrompt();
+});
